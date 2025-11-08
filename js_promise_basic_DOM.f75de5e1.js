a1f893fd05590bf68ed3a9478833939158b59f79
Bohdan Diatliuk <dyatlyuk.bohdan@gmail.com>
2025-11-08 18:12:03 +0200

    make build

---
+++ js_promise_basic_DOM.f75de5e1.js
@@ -0,0 +1,31 @@
+'use strict';
+const promise1 = new Promise((resolve, reject)=>{
+    const setLogo = document.querySelector('.logo');
+    setLogo.addEventListener('click', ()=>{
+        resolve('Promise was resolved!');
+    });
+});
+const promise2 = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        // eslint-disable-next-line prefer-promise-reject-errors
+        reject('Promise 2 reject after 3 seconds!');
+    }, 3000);
+});
+promise1.then((message)=>{
+    // eslint-disable-next-line no-console
+    console.log(message);
+    const div = document.createElement('div');
+    div.className = 'message';
+    div.textContent = 'Promise was resolved!';
+    document.body.appendChild(div);
+});
+promise2.catch((error)=>{
+    // eslint-disable-next-line no-console
+    console.error(error);
+    const div = document.createElement('div');
+    div.className = 'message error-message';
+    div.textContent = 'Promise was rejected!';
+    document.body.appendChild(div);
+});
+
+//# sourceMappingURL=js_promise_basic_DOM.f75de5e1.js.map
